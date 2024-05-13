f0f7eb48778f43c20ff81d713c52edaa2845ed7e
Gwak Beomgyu <67548026+nothingmin@users.noreply.github.com>
2024-05-14 00:12:55 +0900

    Title: 양수 직사각형의 최대 크기, Time: 272ms, Memory: 28MB, Status: Passed - CodeTree

--- 240514/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240514/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -5,12 +5,10 @@ for _ in range(n):
 
 result = -1
 def sum_range(k,i,l,j):
-    sumi = 0
     for a in range(k,k+i):
         for b in range(l, l+j):
-            if maps[a][b] < 0:
+            if maps[a][b] <= 0:
                 return -1
-
     return i*j
 
 
@@ -20,7 +18,5 @@ for i in range(1,n+1):
             for l in range(m):
                 if k+i > n or l+j >m:
                     continue
-                maxi  = sum_range(k,i,l,j)
-                if result <= maxi:
-                    result = maxi 
+                result = max(sum_range(k,i,l,j), result)
 print(result)
