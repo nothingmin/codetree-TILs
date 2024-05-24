4938b96820b6f96ce6d3b3fa096249d70f75e6bf
Gwak Beomgyu <67548026+nothingmin@users.noreply.github.com>
2024-05-24 16:27:10 +0900

    Title: 피보나치 수, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240524/피보나치 수/fibonacci-number.py
+++ 240524/피보나치 수/fibonacci-number.py
@@ -1,19 +1,18 @@
 n = int(input())
-#
-# dp = [-1 for _ in range(n + 1)]
-#
-# dp[0] = 1
-# dp[1] = 1
-# for i in range(2, n + 1):
-#     dp[i] = dp[i - 1] + dp[i - 2]
-# print(dp[n - 1])
 
+
+#
+dp = [-1 for _ in range(n + 1)]
+dp[0] = 1
+dp[1] = 1
 
 def recur(index):
+    if dp[index] != -1:
+        return dp[index]
     if index == 1 or index == 0:
         return 1
 
+    dp[index] = recur(index - 1) + recur(index - 2)
+    return dp[index]
 
-    return recur(index - 1) + recur(index - 2)
-
-print(recur(n-1))
+print(recur(n - 1))
