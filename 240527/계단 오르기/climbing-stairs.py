4f101196ef7f1fd0245de0fb089496c46e153148
Gwak Beomgyu <67548026+nothingmin@users.noreply.github.com>
2024-05-27 18:43:45 +0900

    Title: 계단 오르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240527/계단 오르기/climbing-stairs.py
+++ 240527/계단 오르기/climbing-stairs.py
@@ -1,12 +1,13 @@
 n = int(input())
 dp = [-1 for _ in range(n+1)]
+dp[2] = 1
+dp[3] = 1
+
 def recur(current):
     if not(0 <= current <= n):
         return 0
     if dp[current] != -1:
         return dp[current]    
-    if current == 0:
-        return 1
     dp[current] = recur(current-2) + recur(current-3)
     return dp[current]
 recur(n)
