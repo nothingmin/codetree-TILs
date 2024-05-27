175d3271925bda4431ab6d2f0596d6afb7f68449
Gwak Beomgyu <67548026+nothingmin@users.noreply.github.com>
2024-05-27 18:42:46 +0900

    Title: 계단 오르기, Time: 119ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240527/계단 오르기/climbing-stairs.py
+++ 240527/계단 오르기/climbing-stairs.py
@@ -1,16 +1,13 @@
 n = int(input())
 dp = [-1 for _ in range(n+1)]
-
-
 def recur(current):
-    if not 0<= current <= n:
+    if not(0 <= current <= n):
         return 0
     if dp[current] != -1:
         return dp[current]    
     if current == 0:
         return 1
     dp[current] = recur(current-2) + recur(current-3)
-
     return dp[current]
 recur(n)
 print(dp[n])
