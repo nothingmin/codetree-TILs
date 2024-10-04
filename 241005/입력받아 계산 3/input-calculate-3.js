dae36339eb73bcde6718c9d5c4c4a71bd3e50da2
Gwak Beomgyu <67548026+nothingmin@users.noreply.github.com>
2024-10-05 00:16:54 +0900

    Title: 입력받아 계산 3, Time: 225ms, Memory: 14MB, Status: Passed - CodeTree

--- 241005/입력받아 계산 3/input-calculate-3.js
+++ 241005/입력받아 계산 3/input-calculate-3.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
-let input = fs.readFileSync(0).toSring().split("\n")
+let input = fs.readFileSync(0).toString().split("\n")
 a= Number(input[0])
 b= Number(input[1])
 
-console.log(a*b)
+console.log(`${a*b}`)
