978c198ab92e5010c4fdee2ec5f80b0da41c041c
Gwak Beomgyu <67548026+nothingmin@users.noreply.github.com>
2024-10-10 23:32:42 +0900

    Title: 왕실의 기사 대결, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241010/왕실의 기사 대결/royal-knight-duel.py
+++ 241010/왕실의 기사 대결/royal-knight-duel.py
@@ -34,88 +34,55 @@ def solve():
             r, c, h, w, k = knights[x]
             knights[x] = [r, c - 1, h, w, k]
 
-    def push(new_map, r, c, count, d, pushed):
+    def push(new_map, idx, d, pushed):
+        r, c, h, w, k = knights[idx]
+        directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+        dx, dy = directions[d]
+        for i in range(r, r + h):
+            for j in range(c, c + w):
+                ex, ey = i + dx, j + dy
+                if not (0 <= ex < l and 0 <= ey < l):
+                    return False
+                if new_map[ex][ey] == n:
+                    return False
+                if new_map[ex][ey] == idx:
+                    continue
+                if new_map[ex][ey] != -1:
+                    is_pushed = push(new_map, new_map[ex][ey], d, pushed)
+                    if is_pushed is False:
+                        return False
         if d == 0:
-            for i in range(count):
-                if not (0 <= r - 1 < l and 0 <= c + i < l):
-                    return -1
-                if new_map[r - 1][c + i] == n:
-                    return -1
-                if new_map[r - 1][c + i] != -1:
-                    is_moved = push(new_map, r - 1, c, count, d, pushed)
-                    if is_moved == -1:
-                        return -1
-                    break
-            for i in range(count):
-                if new_map[r][c + i] == -1 or new_map[r][c + i] == n:
-                    continue
-                pushed[new_map[r][c + i]] = True
-                new_map[r - 1][c + i] = new_map[r][c + i]
-                new_map[r][c + i] = -1
+            for i in range(r, r + h):
+                for j in range(c, c + w):
+                    ex, ey = i + dx, j + dy
+                    new_map[ex][ey] = new_map[i][j]
+                    new_map[i][j] = -1
         elif d == 1:
-            for i in range(count):
-                if not (0 <= r + i < l and 0 <= c + 1 < l):
-                    return -1
-                if new_map[r + i][c + 1] == n:
-                    return -1
-                if new_map[r + i][c + 1] != -1:
-                    is_moved = push(new_map, r, c + 1, count, d, pushed)
-                    if is_moved == -1:
-                        return -1
-                    break
-            for i in range(count):
-                if new_map[r + i][c] == -1 or new_map[r + i][c] == n:
-                    continue
-                pushed[new_map[r + i][c]] = True
-                new_map[r + i][c + 1] = new_map[r + i][c]
-                new_map[r + i][c] = -1
+            for j in range(c + w - 1, c - 1, -1):
+                for i in range(r, r + h):
+                    ex, ey = i + dx, j + dy
+                    new_map[ex][ey] = new_map[i][j]
+                    new_map[i][j] = -1
         elif d == 2:
-            for i in range(count):
-                if not (0 <= r + 1 < l and 0 <= c + i < l):
-                    return -1
-                if new_map[r + 1][c + i] == n:
-                    return -1
-                if new_map[r + 1][c + i] != -1:
-                    is_moved = push(new_map, r + 1, c, count, d, pushed)
-                    if is_moved == -1:
-                        return -1
-                    break
-            for i in range(count):
-                if new_map[r][c + i] == -1 or new_map[r][c + i] == n:
-                    continue
-                pushed[new_map[r][c + i]] = True
-                new_map[r + 1][c + i] = new_map[r][c + i]
-                new_map[r][c + i] = -1
+            for i in range(r + h - 1, r - 1, -1):
+                for j in range(c, c + w):
+                    ex, ey = i + dx, j + dy
+                    new_map[ex][ey] = new_map[i][j]
+                    new_map[i][j] = -1
         else:
-            for i in range(count):
-                if not (0 <= r + i < l and 0 <= c - 1 < l):
-                    return -1
-                if new_map[r + i][c - 1] == n:
-                    return -1
-                if new_map[r + i][c - 1] != -1:
-                    is_moved = push(new_map, r, c - 1, count, d, pushed)
-                    if is_moved == -1:
-                        return -1
-                    break
-            for i in range(count):
-                if new_map[r][c + i] == -1 or new_map[r + i][c] == n:
-                    continue
-                pushed[new_map[r + i][c]] = True
-                new_map[r + i][c - 1] = new_map[r + i][c]
-                new_map[r + i][c] = -1
-        return 0
+            for j in range(c, c + w):
+                for i in range(r, r + h):
+                    ex, ey = i + dx, j + dy
+                    new_map[ex][ey] = new_map[i][j]
+                    new_map[i][j] = -1
+        pushed[idx] = True
+        return True
 
-    def move(x, d):
-        r, c, h, w, k = knights[x]
+    def move(new_map, x, d):
         pushed = {}
-        if d == 0:
-            push(maps, r, c, w, d, pushed)
-        elif d == 1:
-            push(maps, r, c, h, d, pushed)
-        elif d == 2:
-            push(maps, r + h - 1, c, w, d, pushed)
-        else:
-            push(maps, r, c + w - 1, h, d, pushed)
+        is_pushed = push(new_map, x, d, pushed)
+        if is_pushed is False:
+            return {}
         for key in pushed.keys():
             update_knigth_pos(key, d)
         return pushed
@@ -126,29 +93,28 @@ def solve():
             for i in range(r, r + h):
                 for j in range(c, c + w):
                     if chess[i][j] == 1:
-                        health[idx]-=1
+                        health[idx] -= 1
 
     total_damage = 0
 
     for _ in range(q):
         x, d = list(map(int, input().split()))
         x = x - 1
-
-        pushed = move(x, d)
-
+        new_map = []
+        for line in maps:
+            new_map.append(line[:])
+        pushed = move(new_map, x, d)
         if len(pushed) == 0:
             continue
- 
+        maps = new_map
         pushed.pop(x)
         calculate_damage(pushed)
 
     for i in range(n):
-        if health[i] <=0:
+        if health[i] <= 0:
             continue
-        total_damage += knights[i][4]- health[i]
+        total_damage += knights[i][4] - health[i]
     print(total_damage)
-
-
 
 
 solve()
